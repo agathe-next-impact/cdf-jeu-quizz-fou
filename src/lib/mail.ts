47c6f5990a210203459ffbb7cdc9bd84737511bd
agathe <agathe@agat.dev>
2026-02-16 09:17:54 +0100

    v1.3 fix mail

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
+    pass: process.env.SMTP_PASS,
   },
 });
 
